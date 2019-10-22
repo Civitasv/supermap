30b386463582fbbe5cd5764072ba56d455a90076
simple <2309780282@qq.com>
2019-10-22 20:06:37 +0800

    合并丁哥可视化代码并完成自然语言处理API的使用

--- src/main/java/cn/hs/utils/MoodUtil.java
+++ src/main/java/cn/hs/utils/MoodUtil.java
@@ -4,16 +4,13 @@ import com.baidu.aip.nlp.AipNlp;
 import org.json.JSONObject;
 
 public class MoodUtil {
-    //设置APPID/AK/SK
-    public static final String APP_ID = "17081433";
-    public static final String API_KEY = "ZDMmw88yjmeYVTeVLjH45Iuw";
-    public static final String SECRET_KEY = "NwwaVlu6HPnShA5rRnkj8Ib2zQ1Vs2iQ";
+    private static final String APP_ID = "17081433";
+    private static final String API_KEY = "ZDMmw88yjmeYVTeVLjH45Iuw";
+    private static final String SECRET_KEY = "NwwaVlu6HPnShA5rRnkj8Ib2zQ1Vs2iQ";
 
     public static JSONObject getMood(String text) {
-        // 初始化一个AipNlp
         AipNlp client = new AipNlp(APP_ID, API_KEY, SECRET_KEY);
 
-        // 可选：设置网络连接参数
         client.setConnectionTimeoutInMillis(2000);
         client.setSocketTimeoutInMillis(60000);
 
